06ccf2d7f5021ce31fe753d337dbf0a80d98fa40
Darc99 <lennyyhugoh@gmail.com>
2022-06-17 00:57:52 +0100

    added task0-2

---
+++ pall.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head;
+
+    (void)(line_number);
+
+	head = *stack;
+	while (head != NULL)
+    {
+        printf("%d\n", head->n);
+        head = head->next;
+        if (head == *stack)
+        {
+            return;
+        }
+        
+    }
+}
